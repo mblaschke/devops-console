6c8ff99a58288caa9a18b1a41e8e0b238bab0da6
Markus Blaschke <mblaschke82@gmail.com>
2020-05-21 23:20:17 +0200

    Fix notification messages
    Signed-off-by: Markus Blaschke <mblaschke82@gmail.com>

--- server.logging.go
+++ server.logging.go
@@ -17,6 +17,14 @@ func (c *Server) auditLog(ctx iris.Context, message string, depth int) {
 }
 
 func (c *Server) notificationMessage(ctx iris.Context, message string) {
+	username := "*anonymous*"
+	user, _ := c.getUser(ctx)
+	if user != nil {
+		username = fmt.Sprintf("%s (%s)", user.Username, user.Uuid)
+	}
+	message = fmt.Sprintf(c.config.App.Notification.Message, username, message)
+
+	// send notification
 	for _, url := range c.config.App.Notification.Channels {
 		if err := shoutrrr.Send(url, message); err != nil {
 			c.logger.Errorf("Unable to send shoutrrr notification: %v", err.Error())
